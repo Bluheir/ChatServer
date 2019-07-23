d3330a0ca5c0580373c6522c340168e7ce516c2d
Christopher <slimcognitodudesvids@outlook.com>
2019-07-23 17:19:15 +1000

    Added SFrameMaskData, a structure for determining the properties of a websocket message

---
+++ ChatServer.ServerHelpers/Methods/SFrameMaskData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatServer.ServerHelpers.Methods
+{
+	public struct SFrameMaskData
+	{
+		public int DataLength, KeyIndex, TotalLenght;
+		public EOpcodeType Opcode;
+
+		public SFrameMaskData(int DataLength, int KeyIndex, int TotalLenght, EOpcodeType Opcode)
+		{
+			this.DataLength = DataLength;
+			this.KeyIndex = KeyIndex;
+			this.TotalLenght = TotalLenght;
+			this.Opcode = Opcode;
+		}
+	}
+}
