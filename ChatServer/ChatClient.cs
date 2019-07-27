5d4b2d05ede5392df83008be1ff097becdedc7f6
Christopher <slimcognitodudesvids@outlook.com>
2019-07-27 10:48:56 +1000

    Added ChatClient.cs as the default client for the server

---
+++ ChatServer/ChatClient.cs
@@ -0,0 +1,16 @@
+﻿using ChatServer.ServerHelpers;
+
+namespace ChatServer
+{
+	public class ChatClient
+	{
+		public WSClient Client { get; }
+		public int ClientId { get; }
+
+		public ChatClient(WSClient client, int clientId)
+		{
+			Client = client;
+			ClientId = clientId;
+		}
+	}
+}
