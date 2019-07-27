6863f542c7b3a8dfabcc677fb30c201ea945c46f
Christopher <slimcognitodudesvids@outlook.com>
2019-07-27 10:52:51 +1000

    Program.cs

---
+++ ChatServer.ChatServerClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace ChatServer.ChatServerClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		=> new ChatClient
+		(
+			new Uri("ws://localhost"),
+			new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080),
+			Encoding.UTF8
+		).StartAsync().GetAwaiter().GetResult();
+	}
+}
